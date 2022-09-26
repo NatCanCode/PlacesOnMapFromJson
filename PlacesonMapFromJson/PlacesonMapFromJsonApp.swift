c3f43138cff53feb7dc214a708a98118d837f492
NatCanCode <nathalie.piat1@gmail.com>
2022-09-26 10:43:20 +0200

    Initial Commit

---
+++ PlacesonMapFromJson/PlacesonMapFromJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesonMapFromJsonApp.swift
+//  PlacesonMapFromJson
+//
+//  Created by N N on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlacesonMapFromJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
